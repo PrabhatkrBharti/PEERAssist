45ef73053d9015d926b49c860d2e4232ea30175d
PrabhatkrBharti <59766117+PrabhatkrBharti@users.noreply.github.com>
2021-04-02 06:49:09 +0530

    Update encoder.sh

--- encoder.sh
+++ encoder.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ $# == 0 ]
+	then
+		echo "ERROR : provide a year ,e.g. ./encoder.sh 2017"
+		exit
+fi
 
 echo "......GENERATING SECTIONAL-SUMMARY......"
 python summaFlatten.py "$1"
